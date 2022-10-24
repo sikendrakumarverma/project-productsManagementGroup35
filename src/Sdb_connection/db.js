4d453990e2d4cedecbb0f9e5a9f3c37377055ad0
sikendrakumarverma <grdskverma2018@gmail.com>
2022-10-24 19:08:28 +0530

    tested

--- src/Sdb_connection/db.js
+++ src/Sdb_connection/db.js
@@ -3,10 +3,11 @@ require('dotenv').config();
 
 module.exports = () => {
 	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
+		useNewUrlParser: true,  // allow users to fall back to the old parser if they find a bug in the new parser.
+		useUnifiedTopology: true, // DeprecationWarning: current Server Discovery and Monitoring engine is deprecated, 
+		//and will be removed in a future version
 	};
-	
+
 	try {
 		mongoose.connect(process.env.DB, connectionParams);
 		console.log("Connected to database successfully");
